6ba7c5736367faa46011219362c68027c562a689
Cauê Santos <cauedossantossilva@hotmail.com>
2022-07-28 09:49:49 -0300

    test5

--- hooks/pre-commit-asksuite.sh
+++ hooks/pre-commit-asksuite.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_branch_regex="$(curl https://bitbucket.org/api/2.0/snippets/cauesanttos/9ELpjA/ca712cd47df72f5ef4c9b632db8974021227a159/files/branch-name)"
+valid_branch_regex='^((fix|feat)\/[a-zA-Z0-9\-]+)$'
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
